8a8aeb64d40c1fb2ba630fe968799154f0ec7c69
yoksoup <89482694+yoksoup@users.noreply.github.com>
2021-12-05 18:28:25 -0600

    pages upload

---
+++ Project/kfinleyITIN8000Scout/src/pages/views.py
@@ -0,0 +1,32 @@
+"""
+------------------------------------------------------------
+Some test pages that I was working on plus the Home page
+------------------------------------------------------------
+"""
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request, *args, **kwargs): # *args, **kwargs
+    print(args, kwargs)
+    print(request.user)
+    return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "title": "abc this is about us",
+        "this_is_true": True,
+        "my_number": 123,
+        "my_list": [1313, 4231, 312, "Abc"],
+        "my_html": "<h1>Hello World</h1>"
+
+    }
+    return render(request, "about.html", my_context)
+
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Social Page</h1>")
